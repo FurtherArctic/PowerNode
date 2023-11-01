a56e6bfe753947dcc4c8642b3d8b1b1373b8734f
wangjunchen <wangjunchen2017@outlook.com>
2023-11-01 16:49:38 +0800

    Jar包的打包，导入与撤销

---
+++ stage3_web_socket/class01_JDBC/class01_provide_jar/src/com/powernode/entity/Student.java
@@ -0,0 +1,7 @@
+package com.powernode.entity;
+
+public class Student {
+    public int add(int p1, int p2) {
+        return p1 + p2;
+    }
+}
---
+++ stage3_web_socket/class01_JDBC/class02_import_jar/src/com/powernode/test/TestMain.java
@@ -0,0 +1,10 @@
+package com.powernode.test;
+
+import com.powernode.entity.Student;
+
+public class TestMain {
+    public static void main(String[] args) {
+        Student student=new Student();
+        System.out.println(student.add(12, 23));
+    }
+}
