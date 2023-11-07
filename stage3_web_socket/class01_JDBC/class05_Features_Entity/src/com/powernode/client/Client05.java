5fece48364a46cf9dfa8d8cc88f3da81669e873f
wangjunchen <wangjunchen2017@outlook.com>
2023-11-07 16:22:09 +0800

    基于特征封装-Entity类

---
+++ stage3_web_socket/class01_JDBC/class05_Features_Entity/src/com/powernode/client/Client05.java
@@ -0,0 +1,81 @@
+package com.powernode.client;
+
+import com.powernode.dao.DeptDao;
+import com.powernode.entity.Dept;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Client05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DeptDao deptDao = new DeptDao();
+        System.out.println("1. insert");
+        System.out.println("2. update");
+        System.out.println("3. delete");
+        System.out.println("4. showAll");
+        int flag = scanner.nextInt();
+        int deptNo, result;
+        String deptName;
+        String location;
+
+        switch (flag) {
+            case 1 -> {
+                System.out.println("please enter deptNo:");
+                deptNo = scanner.nextInt();
+                System.out.println("please enter deptName:");
+                deptName = scanner.next();
+                System.out.println("please enter location:");
+                location = scanner.next();
+                try {
+                    result = deptDao.insertDept(new Dept(deptNo, deptName, location));
+                    System.out.println(result + " new user has been registered");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println("Error!!!");
+                }
+            }
+            case 2 -> {
+                System.out.println("please enter deptNo:");
+                deptNo = scanner.nextInt();
+                System.out.println("please enter deptName:");
+                deptName = scanner.next();
+                System.out.println("please enter location:");
+                location = scanner.next();
+                try {
+                    result = deptDao.updateDao(new Dept(deptNo, deptName, location));
+                    System.out.println(result + " user has been updated");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println("Error!!!");
+                }
+            }
+            case 3 -> {
+                System.out.println("please enter deptNo:");
+                deptNo = scanner.nextInt();
+                try {
+                    result = deptDao.deleteDao(deptNo);
+                    System.out.println(result + " user has been deleted");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println("Error");
+                }
+            }
+            case 4 -> {
+                System.out.println("show all information");
+                try {
+                    List<Dept> deptList = deptDao.findAll();
+                    for (Iterator<Dept> iterator = deptList.iterator(); iterator.hasNext(); ) {
+                        Dept dept = iterator.next();
+                        System.out.println(dept);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            default -> System.out.println("error!!!!");
+        }
+    }
+}
---
+++ stage3_web_socket/class01_JDBC/class05_Features_Entity/src/com/powernode/dao/DeptDao.java
@@ -0,0 +1,152 @@
+package com.powernode.dao;
+
+import com.powernode.entity.Dept;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wangjunchen
+ * 基于流程的封装Dept表操作
+ * insert与update方法的参数个数随着字段个数增加而无限增加，开发负担加重，使用难度增加
+ * 因此可以使用 Entity 类来解决此问题
+ */
+public class DeptDao {
+    public static String username = "root";
+    public static String password = "123456";
+    public static String url = "jdbc:mysql://192.168.0.107:3306/power_node?serverTimezone=UTC";
+
+    public Connection conn;
+    public PreparedStatement ps;
+
+
+    /**
+     * 插入语句的封装
+     *
+     * @param dept 部门信息
+     * @return int
+     * @throws ClassNotFoundException 异常
+     * @throws SQLException           异常
+     */
+    public int insertDept(Dept dept) throws Exception {
+
+        String sql = "INSERT INTO dept values (?,?,?)";
+        int result;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, username, password);
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, dept.getDeptNo());
+            ps.setString(2, dept.getdName());
+            ps.setString(3, dept.getLoc());
+            result = ps.executeUpdate();
+        } finally {
+            if (ps != null) {
+                ps.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 删除语句的封装
+     *
+     * @param deptNo 部门编号
+     * @return int
+     * @throws Exception 异常
+     */
+    public int deleteDao(int deptNo) throws Exception {
+
+        String sql = "delete from dept where DEPTNO=?";
+        int result;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, username, password);
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, deptNo);
+            result = ps.executeUpdate();
+        } finally {
+            if (ps != null) {
+                ps.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+
+
+        return result;
+    }
+
+    /**
+     * 更新语句的封装
+     *
+     * @param dept 部门信息
+     * @return int
+     * @throws Exception 异常
+     */
+    public int updateDao(Dept dept) throws Exception {
+
+        String sql = "update  dept set DNAME=?,LOC=? where DEPTNO=?";
+        int result;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, username, password);
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, dept.getdName());
+            ps.setString(2, dept.getLoc());
+            ps.setInt(3, dept.getDeptNo());
+            result = ps.executeUpdate();
+        } finally {
+            if (ps != null) {
+                ps.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 查询全部数据
+     *
+     * @return dept 集合
+     * @throws Exception 异常
+     */
+    public List<Dept> findAll() throws Exception {
+        ResultSet rs = null;
+        List<Dept> deptList = new ArrayList<Dept>();
+        String sql = "select * from dept";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, username, password);
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int deptNo = rs.getInt("deptNo");
+                String deptName = rs.getString("dName");
+                String loc = rs.getString("loc");
+                Dept dept = new Dept(deptNo, deptName, loc);
+                deptList.add(dept);
+            }
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return deptList;
+    }
+}
