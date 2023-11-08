8ea8a83932d70ebdb1765cb12e313263b005db0a
wangjunchen <wangjunchen2017@outlook.com>
2023-11-08 17:01:02 +0800

    基于功能封装-Util类

---
+++ stage3_web_socket/class01_JDBC/class06_Function_Util/src/com/powernode/entity/Dept.java
@@ -0,0 +1,49 @@
+package com.powernode.entity;
+
+public class Dept {
+    private Integer deptNo;
+    private String dName;
+    private String loc;
+
+    public Integer getDeptNo() {
+        return deptNo;
+    }
+
+    public void setDeptNo(Integer deptNo) {
+        this.deptNo = deptNo;
+    }
+
+    public String getdName() {
+        return dName;
+    }
+
+    public void setdName(String dName) {
+        this.dName = dName;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
+
+    public Dept(Integer deptNo, String dName, String loc) {
+        this.deptNo = deptNo;
+        this.dName = dName;
+        this.loc = loc;
+    }
+
+    public Dept() {
+    }
+
+    @Override
+    public String toString() {
+        return "Dept{" +
+                "deptNo=" + deptNo +
+                ", deptName='" + dName + '\'' +
+                ", loc='" + loc + '\'' +
+                '}';
+    }
+}
